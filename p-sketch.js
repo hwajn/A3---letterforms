1742eb2d9d87c75c56b66dbd0cef15796ec6b291
hwajn <64378385+hwajn@users.noreply.github.com>
2021-03-12 16:38:57 -0500

    test

---
+++ p-sketch.js
@@ -0,0 +1,92 @@
+function setup() {
+    createCanvas(1000, 1000, WEBGL);
+  }
+  
+
+  function draw() {
+    
+
+    // a rotating cube! https://p5js.org/reference/#/p5/rotateX
+    rotateX(frameCount * .1);
+    background(200);
+    
+    //top stroke
+    
+    box(20,100,20);
+    stroke(0);
+    strokeWeight(10);
+    translate(20, 0,0);
+    rotateX(10);
+    
+
+    box(20,100,20);
+    translate(20, 0,0);
+    rotateX(10);
+
+
+    box(20,100,20);
+    translate(20, 0,0);
+    rotateX(10);
+    
+    box(20,100,20);
+    translate(20, 0,0);
+    rotateX(10);
+
+
+    box(20,100,20);
+    translate(20, 0,0);
+    rotateX(10);
+    
+    box(20,100,20);
+    translate(20, 0,0);
+    rotateX(10);
+    
+    box(20,100,20);
+    translate(20, 0,0);
+    rotateX(10);
+    
+    box(20,100,20);
+    translate(20, 0,0);
+    rotateX(10);
+    
+    box(20,100,20);
+    translate(0, 0,0);
+    rotateX(10);
+    
+
+
+    
+    box(20,100,20);
+    translate(20, 0,0);
+    rotateX(10);
+        
+    box(20,100,20);
+    translate(20, 0,0);
+    rotateX(10);
+
+
+    box(20,100,20);
+    translate(20, 0,0);
+    rotateX(10);
+    
+    box(20,100,20);
+    translate(20, 0,0);
+    rotateX(10);
+
+
+    box(20,100,20);
+    translate(20, 0,0);
+    rotateX(10);
+    
+    box(20,100,20);
+    translate(20, 0,0);
+    rotateX(10);    
+
+
+
+    push();
+// a rotating cube! https://p5js.org/reference/#/p5/rotateX
+
+
+
+  }
