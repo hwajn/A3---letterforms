5582c02c19cf8faf210d1de62cc333689194b986
hwajn <64378385+hwajn@users.noreply.github.com>
2021-03-13 21:40:09 -0500

    L-1

--- p-sketch.js
+++ p-sketch.js
@@ -1,92 +1,1050 @@
 function setup() {
-    createCanvas(1000, 1000, WEBGL);
-  }
+  createCanvas(1000, 1000, WEBGL);
   
-
-  function draw() {
-    
-
-    // a rotating cube! https://p5js.org/reference/#/p5/rotateX
-    rotateX(frameCount * .1);
-    background(200);
-    
-    //top stroke
-    
-    box(20,100,20);
-    stroke(0);
-    strokeWeight(10);
-    translate(20, 0,0);
-    rotateX(10);
-    
-
-    box(20,100,20);
-    translate(20, 0,0);
-    rotateX(10);
-
-
-    box(20,100,20);
-    translate(20, 0,0);
-    rotateX(10);
-    
-    box(20,100,20);
-    translate(20, 0,0);
-    rotateX(10);
-
-
-    box(20,100,20);
-    translate(20, 0,0);
-    rotateX(10);
-    
-    box(20,100,20);
-    translate(20, 0,0);
-    rotateX(10);
-    
-    box(20,100,20);
-    translate(20, 0,0);
-    rotateX(10);
-    
-    box(20,100,20);
-    translate(20, 0,0);
-    rotateX(10);
-    
-    box(20,100,20);
-    translate(0, 0,0);
-    rotateX(10);
-    
-
-
-    
-    box(20,100,20);
-    translate(20, 0,0);
-    rotateX(10);
-        
-    box(20,100,20);
-    translate(20, 0,0);
-    rotateX(10);
-
-
-    box(20,100,20);
-    translate(20, 0,0);
-    rotateX(10);
-    
-    box(20,100,20);
-    translate(20, 0,0);
-    rotateX(10);
-
-
-    box(20,100,20);
-    translate(20, 0,0);
-    rotateX(10);
-    
-    box(20,100,20);
-    translate(20, 0,0);
-    rotateX(10);    
+}
 
 
 
-    push();
-// a rotating cube! https://p5js.org/reference/#/p5/rotateX
+
+function draw() 
+{
+
+//rectangles 1, column 1
+push();
+angleMode(DEGREES);
+translate(-300,-200);
+background(200);
+rotateY(100);
+
+
+push();
+fill(0);
+stroke(255, 255, 255)
+
+rotateX(millis() /  30);
+
+box(200,30,40);  
+pop();
+
+push();
+fill(0);
+
+rotateX(millis() /  -30);
+stroke(255, 255, 255)
+box(200,30,40); 
+pop();
+
+push();
+fill(0);
+stroke(255, 255, 255)
+
+rotateX(millis() /  15);
+
+box(200,30,40);  
+pop();
+
+push();
+fill(0);
+
+rotateX(millis() /  -15);
+stroke(255, 255, 255)
+box(200,30,40); 
+pop();
+
+
+push();
+fill(0);
+stroke(255, 255, 255)
+
+rotateX(millis() /  7);
+
+box(200,30,40);  
+pop();
+
+push();
+fill(0);
+
+rotateX(millis() /  -7);
+stroke(255, 255, 255)
+box(200,30,40); 
+pop();
+
+pop();
+
+//rectangles 2
+
+push();
+angleMode(DEGREES);
+translate(-300,-180);
+rotateY(100);
+
+
+push();
+fill(0);
+stroke(255, 255, 255)
+
+rotateX(millis() /  30);
+
+box(200,30,40);  
+pop();
+
+push();
+fill(0);
+
+rotateX(millis() /  -30);
+stroke(255, 255, 255)
+box(200,30,40); 
+pop();
+
+push();
+fill(0);
+stroke(255, 255, 255)
+
+rotateX(millis() /  15);
+
+box(200,30,40);  
+pop();
+
+push();
+fill(0);
+
+rotateX(millis() /  -15);
+stroke(255, 255, 255)
+box(200,30,40); 
+pop();
+
+
+push();
+fill(0);
+stroke(255, 255, 255)
+
+rotateX(millis() /  7);
+
+box(200,30,40);  
+pop();
+
+push();
+fill(0);
+
+rotateX(millis() /  -7);
+stroke(255, 255, 255)
+box(200,30,40); 
+pop();
+pop();
+
+
+//rectangles 3
+
+
+push();
+angleMode(DEGREES);
+translate(-300,-160);
+rotateY(100);
+
+
+push();
+fill(0);
+stroke(255, 255, 255)
+
+rotateX(millis() /  30);
+
+box(200,30,40);  
+pop();
+
+push();
+fill(0);
+
+rotateX(millis() /  -30);
+stroke(255, 255, 255)
+box(200,30,40); 
+pop();
+
+push();
+fill(0);
+stroke(255, 255, 255)
+
+rotateX(millis() /  15);
+
+box(200,30,40);  
+pop();
+
+push();
+fill(0);
+
+rotateX(millis() /  -15);
+stroke(255, 255, 255)
+box(200,30,40); 
+pop();
+
+
+push();
+fill(0);
+stroke(255, 255, 255)
+
+rotateX(millis() /  7);
+
+box(200,30,40);  
+pop();
+
+push();
+fill(0);
+
+rotateX(millis() /  -7);
+stroke(255, 255, 255)
+box(200,30,40); 
+pop();
+pop();
+
+
+//rectangles 4
+
+
+push();
+angleMode(DEGREES);
+translate(-300,-140);
+rotateY(100);
+
+
+push();
+fill(0);
+stroke(255, 255, 255)
+
+rotateX(millis() /  30);
+
+box(200,30,40);  
+pop();
+
+push();
+fill(0);
+
+rotateX(millis() /  -30);
+stroke(255, 255, 255)
+box(200,30,40); 
+pop();
+
+push();
+fill(0);
+stroke(255, 255, 255)
+
+rotateX(millis() /  15);
+
+box(200,30,40);  
+pop();
+
+push();
+fill(0);
+
+rotateX(millis() /  -15);
+stroke(255, 255, 255)
+box(200,30,40); 
+pop();
+
+
+push();
+fill(0);
+stroke(255, 255, 255)
+
+rotateX(millis() /  7);
+
+box(200,30,40);  
+pop();
+
+push();
+fill(0);
+
+rotateX(millis() /  -7);
+stroke(255, 255, 255)
+box(200,30,40); 
+pop();
+pop();
 
 
 
-  }
+
+//rectangles 5
+
+
+push();
+angleMode(DEGREES);
+translate(-300,-120);
+rotateY(100);
+
+
+push();
+fill(0);
+stroke(255, 255, 255)
+
+rotateX(millis() /  30);
+
+box(200,30,40);  
+pop();
+
+push();
+fill(0);
+
+rotateX(millis() /  -30);
+stroke(255, 255, 255)
+box(200,30,40); 
+pop();
+
+push();
+fill(0);
+stroke(255, 255, 255)
+
+rotateX(millis() /  15);
+
+box(200,30,40);  
+pop();
+
+push();
+fill(0);
+
+rotateX(millis() /  -15);
+stroke(255, 255, 255)
+box(200,30,40); 
+pop();
+
+
+push();
+fill(0);
+stroke(255, 255, 255)
+
+rotateX(millis() /  7);
+
+box(200,30,40);  
+pop();
+
+push();
+fill(0);
+
+rotateX(millis() /  -7);
+stroke(255, 255, 255)
+box(200,30,40); 
+pop();
+pop();
+
+
+
+
+//rectangles 6
+
+
+push();
+angleMode(DEGREES);
+translate(-300,-100);
+rotateY(100);
+
+
+push();
+fill(0);
+stroke(255, 255, 255)
+
+rotateX(millis() /  30);
+
+box(200,30,40);  
+pop();
+
+push();
+fill(0);
+
+rotateX(millis() /  -30);
+stroke(255, 255, 255)
+box(200,30,40); 
+pop();
+
+push();
+fill(0);
+stroke(255, 255, 255)
+
+rotateX(millis() /  15);
+
+box(200,30,40);  
+pop();
+
+push();
+fill(0);
+
+rotateX(millis() /  -15);
+stroke(255, 255, 255)
+box(200,30,40); 
+pop();
+
+
+push();
+fill(0);
+stroke(255, 255, 255)
+
+rotateX(millis() /  7);
+
+box(200,30,40);  
+pop();
+
+push();
+fill(0);
+
+rotateX(millis() /  -7);
+stroke(255, 255, 255)
+box(200,30,40); 
+pop();
+pop();
+
+
+
+//rectangles 7
+
+
+push();
+angleMode(DEGREES);
+translate(-300,-80);
+rotateY(100);
+
+
+push();
+fill(0);
+stroke(255, 255, 255)
+
+rotateX(millis() /  30);
+
+box(200,30,40);  
+pop();
+
+push();
+fill(0);
+
+rotateX(millis() /  -30);
+stroke(255, 255, 255)
+box(200,30,40); 
+pop();
+
+push();
+fill(0);
+stroke(255, 255, 255)
+
+rotateX(millis() /  15);
+
+box(200,30,40);  
+pop();
+
+push();
+fill(0);
+
+rotateX(millis() /  -15);
+stroke(255, 255, 255)
+box(200,30,40); 
+pop();
+
+
+push();
+fill(0);
+stroke(255, 255, 255)
+
+rotateX(millis() /  7);
+
+box(200,30,40);  
+pop();
+
+push();
+fill(0);
+rotateX(millis() /  -7);
+stroke(255, 255, 255)
+box(200,30,40); 
+pop();
+pop();
+
+
+
+
+
+//rectangles 8
+
+
+push();
+angleMode(DEGREES);
+translate(-300,-60);
+rotateY(100);
+
+
+push();
+fill(0);
+stroke(255, 255, 255)
+
+rotateX(millis() /  30);
+
+box(200,30,40);  
+pop();
+
+push();
+fill(0);
+
+rotateX(millis() /  -30);
+stroke(255, 255, 255)
+box(200,30,40); 
+pop();
+
+push();
+fill(0);
+stroke(255, 255, 255)
+
+rotateX(millis() /  15);
+
+box(200,30,40);  
+pop();
+
+push();
+fill(0);
+
+rotateX(millis() /  -15);
+stroke(255, 255, 255)
+box(200,30,40); 
+pop();
+
+
+push();
+fill(0);
+stroke(255, 255, 255)
+
+rotateX(millis() /  7);
+
+box(200,30,40);  
+pop();
+
+push();
+fill(0);
+rotateX(millis() /  -7);
+stroke(255, 255, 255)
+box(200,30,40); 
+pop();
+pop();
+
+
+
+
+
+//rectangles 9
+
+
+push();
+angleMode(DEGREES);
+translate(-300,-40);
+rotateY(100);
+
+
+push();
+fill(0);
+stroke(255, 255, 255)
+
+rotateX(millis() /  30);
+
+box(200,30,40);  
+pop();
+
+push();
+fill(0);
+
+rotateX(millis() /  -30);
+stroke(255, 255, 255)
+box(200,30,40); 
+pop();
+
+push();
+fill(0);
+stroke(255, 255, 255)
+
+rotateX(millis() /  15);
+
+box(200,30,40);  
+pop();
+
+push();
+fill(0);
+
+rotateX(millis() /  -15);
+stroke(255, 255, 255)
+box(200,30,40); 
+pop();
+
+
+push();
+fill(0);
+stroke(255, 255, 255)
+
+rotateX(millis() /  7);
+
+box(200,30,40);  
+pop();
+
+push();
+fill(0);
+rotateX(millis() /  -7);
+stroke(255, 255, 255)
+box(200,30,40); 
+pop();
+pop();
+
+
+
+//rectangles 10
+
+
+push();
+angleMode(DEGREES);
+translate(-300,-20);
+rotateY(100);
+
+
+push();
+fill(0);
+stroke(255, 255, 255)
+
+rotateX(millis() /  30);
+
+box(200,30,40);  
+pop();
+
+push();
+fill(0);
+
+rotateX(millis() /  -30);
+stroke(255, 255, 255)
+box(200,30,40); 
+pop();
+
+push();
+fill(0);
+stroke(255, 255, 255)
+
+rotateX(millis() /  15);
+
+box(200,30,40);  
+pop();
+
+push();
+fill(0);
+
+rotateX(millis() /  -15);
+stroke(255, 255, 255)
+box(200,30,40); 
+pop();
+
+
+push();
+fill(0);
+stroke(255, 255, 255)
+
+rotateX(millis() /  7);
+
+box(200,30,40);  
+pop();
+
+push();
+fill(0);
+rotateX(millis() /  -7);
+stroke(255, 255, 255)
+box(200,30,40); 
+pop();
+pop();
+
+
+
+
+
+
+//rectangles 11
+
+
+push();
+angleMode(DEGREES);
+translate(-300,-0);
+rotateY(100);
+
+
+push();
+fill(0);
+stroke(255, 255, 255)
+
+rotateX(millis() /  30);
+
+box(200,30,40);  
+pop();
+
+push();
+fill(0);
+
+rotateX(millis() /  -30);
+stroke(255, 255, 255)
+box(200,30,40); 
+pop();
+
+push();
+fill(0);
+stroke(255, 255, 255)
+
+rotateX(millis() /  15);
+
+box(200,30,40);  
+pop();
+
+push();
+fill(0);
+
+rotateX(millis() /  -15);
+stroke(255, 255, 255)
+box(200,30,40); 
+pop();
+
+
+push();
+fill(0);
+stroke(255, 255, 255)
+
+rotateX(millis() /  7);
+
+box(200,30,40);  
+pop();
+
+push();
+fill(0);
+rotateX(millis() /  -7);
+stroke(255, 255, 255)
+box(200,30,40); 
+pop();
+pop();
+
+
+
+
+
+//rectangles 12
+
+
+push();
+angleMode(DEGREES);
+translate(-300,25);
+rotateY(100);
+
+
+push();
+fill(0);
+stroke(255, 255, 255)
+
+rotateX(millis() /  30);
+
+box(200,30,40);  
+pop();
+
+push();
+fill(0);
+
+rotateX(millis() /  -30);
+stroke(255, 255, 255)
+box(200,30,40); 
+pop();
+
+push();
+fill(0);
+stroke(255, 255, 255)
+
+rotateX(millis() /  15);
+
+box(200,30,40);  
+pop();
+
+push();
+fill(0);
+
+rotateX(millis() /  -15);
+stroke(255, 255, 255)
+box(200,30,40); 
+pop();
+
+
+push();
+fill(0);
+stroke(255, 255, 255)
+
+rotateX(millis() /  7);
+
+box(200,30,40);  
+pop();
+
+push();
+fill(0);
+rotateX(millis() /  -7);
+stroke(255, 255, 255)
+box(200,30,40); 
+pop();
+pop();
+
+
+
+
+
+
+
+
+
+
+            //rectangles 1, BOTTOM
+
+            
+
+            push();
+            angleMode(DEGREES);
+            translate(-280,25);
+            rotateY(100);
+
+
+            push();
+            fill(0);
+            stroke(255, 255, 255)
+
+            rotateX(millis() /  30);
+
+            box(200,30,40);  
+            pop();
+
+            push();
+            fill(0);
+
+            rotateX(millis() /  -30);
+            stroke(255, 255, 255)
+            box(200,30,40); 
+            pop();
+
+            push();
+            fill(0);
+            stroke(255, 255, 255)
+
+            rotateX(millis() /  15);
+
+            box(200,30,40);  
+            pop();
+
+            push();
+            fill(0);
+
+            rotateX(millis() /  -15);
+            stroke(255, 255, 255)
+            box(200,30,40); 
+            pop();
+
+
+            push();
+            fill(0);
+            stroke(255, 255, 255)
+
+            rotateX(millis() /  7);
+
+            box(200,30,40);  
+            pop();
+
+            push();
+            fill(0);
+            rotateX(millis() /  -7);
+            stroke(255, 255, 255)
+            box(200,30,40); 
+            pop();
+            pop();
+
+
+
+
+             //rectangles 2, BOTTOM
+
+            
+
+             push();
+             angleMode(DEGREES);
+             translate(-260,25);
+             rotateY(100);
+ 
+ 
+             push();
+             fill(0);
+             stroke(255, 255, 255)
+ 
+             rotateX(millis() /  30);
+ 
+             box(200,30,40);  
+             pop();
+ 
+             push();
+             fill(0);
+ 
+             rotateX(millis() /  -30);
+             stroke(255, 255, 255)
+             box(200,30,40); 
+             pop();
+ 
+             push();
+             fill(0);
+             stroke(255, 255, 255)
+ 
+             rotateX(millis() /  15);
+ 
+             box(200,30,40);  
+             pop();
+ 
+             push();
+             fill(0);
+ 
+             rotateX(millis() /  -15);
+             stroke(255, 255, 255)
+             box(200,30,40); 
+             pop();
+ 
+ 
+             push();
+             fill(0);
+             stroke(255, 255, 255)
+ 
+             rotateX(millis() /  7);
+ 
+             box(200,30,40);  
+             pop();
+ 
+             push();
+             fill(0);
+             rotateX(millis() /  -7);
+             stroke(255, 255, 255)
+             box(200,30,40); 
+             pop();
+             pop();
+
+
+
+
+
+
+             //rectangles 3, BOTTOM
+
+            
+
+             push();
+             angleMode(DEGREES);
+             translate(-240,25);
+             rotateY(100);
+ 
+ 
+             push();
+             fill(0);
+             stroke(255, 255, 255)
+ 
+             rotateX(millis() /  30);
+ 
+             box(200,30,40);  
+             pop();
+ 
+             push();
+             fill(0);
+ 
+             rotateX(millis() /  -30);
+             stroke(255, 255, 255)
+             box(200,30,40); 
+             pop();
+ 
+             push();
+             fill(0);
+             stroke(255, 255, 255)
+ 
+             rotateX(millis() /  15);
+ 
+             box(200,30,40);  
+             pop();
+ 
+             push();
+             fill(0);
+ 
+             rotateX(millis() /  -15);
+             stroke(255, 255, 255)
+             box(200,30,40); 
+             pop();
+ 
+ 
+             push();
+             fill(0);
+             stroke(255, 255, 255)
+ 
+             rotateX(millis() /  7);
+ 
+             box(200,30,40);  
+             pop();
+ 
+             push();
+             fill(0);
+             rotateX(millis() /  -7);
+             stroke(255, 255, 255)
+             box(200,30,40); 
+             pop();
+             pop();
+
+
+
+
+
+
+             //rectangles 4, BOTTOM
+
+            
+
+             push();
+             angleMode(DEGREES);
+             translate(-220,25);
+             rotateY(100);
+             rotateZ(-2)
+ 
+ 
+             push();
+             fill(0);
+             stroke(255, 255, 255)
+ 
+             rotateX(millis() /  30);
+ 
+             box(200,30,40);  
+             pop();
+ 
+             push();
+             fill(0);
+ 
+             rotateX(millis() /  -30);
+             stroke(255, 255, 255)
+             box(200,30,40); 
+             pop();
+ 
+             push();
+             fill(0);
+             stroke(255, 255, 255)
+ 
+             rotateX(millis() /  15);
+ 
+             box(200,30,40);  
+             pop();
+ 
+             push();
+             fill(0);
+ 
+             rotateX(millis() /  -15);
+             stroke(255, 255, 255)
+             box(200,30,40); 
+             pop();
+ 
+ 
+             push();
+             fill(0);
+             stroke(255, 255, 255)
+ 
+             rotateX(millis() /  7);
+ 
+             box(200,30,40);  
+             pop();
+ 
+             push();
+             fill(0);
+             rotateX(millis() /  -7);
+             stroke(255, 255, 255)
+             box(200,30,40); 
+             pop();
+             pop();
+
+
+}
