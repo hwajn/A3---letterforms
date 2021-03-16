81238781e8cddfa111ee0e78fe553d88f0f5c8ff
hwajn <64378385+hwajn@users.noreply.github.com>
2021-03-16 19:00:19 -0400

    L no 6

--- p-sketch.js
+++ p-sketch.js
@@ -1,63 +1,36 @@
 
+
+let distribution = new Array(360);
 function setup() {
   createCanvas(1000, 1000);
   angleMode(DEGREES);
-
-stroke(0);
-noFill();
-strokeJoin(MITER);
-  }
-
-//https://www.youtube.com/watch?v=kP-RkS70Lm8
-
-
-
-function draw() {
   
-background(1000);
-translate(width/2,-20);
+  translate(400,900);
+  push();
+  rotate(-45);
+
+  
+
+
 
 push();
-strokeWeight(2);
-
-
-for (let i = 0; i < 5000; i++){
-
-  beginShape();
-line(0,0,0,300);
-  endShape();
-
-scale(.9,1);
-translate(i*1,0);
-shearY(-1);
-
-
-
-
-
-}
+scale(5);
+  for(var i = 0; i <2000000; i++) {
+    let r = random(30);
+    ellipse(r*35, r/1000, 2000, 2000);
+  scale(.999);
+  stroke(255,255,255);
+  strokeWeight(2);
+fill(0);
+  }
 pop();
-push();
-strokeWeight(.3);
-scale(.5,2);
-translate(50,-145);
-for (let i = 0; i < 10; i++){
 
 
-line(-50,300,340,300);
-scale(1,1);
-translate(i*1,-.00009);
-shearY(-.5);
 
 
-}    pop();                         
-                              
+
+
 }
-
-
-
-
-
   
 
 
