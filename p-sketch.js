7dd747a141cefd584efa7c552707e731b3a64287
hwajn <64378385+hwajn@users.noreply.github.com>
2021-03-16 20:19:30 -0400

    test

--- p-sketch.js
+++ p-sketch.js
@@ -1,43 +1,13 @@
-
-
-let distribution = new Array(360);
-function setup() {
-  createCanvas(1000, 1000);
-  angleMode(DEGREES);
-  
-  translate(400,900);
-  push();
-  rotate(-45);
-
-  
-
-
-
-push();
-scale(5);
-  for(var i = 0; i <2000000; i++) {
-    let r = random(30);
-    ellipse(r*35, r/1000, 2000, 2000);
-  scale(.999);
-  stroke(255,255,255);
-  strokeWeight(2);
-fill(0);
-  }
-pop();
-
-
-
-
-
-
+var myFont;
+function preload() {
+  myFont = loadFont('WindNE.ttf');
 }
-  
 
-
-
-
-
-
-
-
-
+function setup() { 
+  createCanvas(800, 500);
+	background(230);
+	fill(255, 0, 0);
+	textSize(100);
+	textFont(myFont);
+	text("Rune Madsen", 150, height/2);
+} 
