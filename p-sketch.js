b741c9620d3f3615ff20a291d6ce2cb593ae3d72
hwajn <64378385+hwajn@users.noreply.github.com>
2021-03-17 00:09:40 -0400

    test

--- p-sketch.js
+++ p-sketch.js
@@ -1,13 +1,38 @@
-var myFont;
+//https://editor.p5js.org/runemadsen/sketches/B1rJPV7n-
+
+
+let grotesk;
+let hiArray;
+letFontSize = 200;
+
+
 function preload() {
-  myFont = loadFont('WindNE.ttf');
+  grotesk = loadFont('grotesk.ttf');
+  grotesk = createFont("grotesk.ttf");
+  
 }
 
 function setup() { 
-  createCanvas(800, 500);
-	background(230);
-	fill(255, 0, 0);
-	textSize(100);
-	textFont(myFont);
-	text("Rune Madsen", 150, height/2);
+  createCanvas(1000, 1000);
+	noBackground();
+	fill(255,255,255);
+  stroke(0);
+	textSize(500);
+	textFont(grotesk);
+	textSize(fontSize);
+
+``
+  let hiArray = grotesk.textToPoints("L",width/2, height/2,fontSize);
+  print(hiArray);
 } 
+
+
+
+function draw(){
+  noBackground();
+	fill(255,255,255);
+  stroke(0);
+text("L",width/2, height/2);
+for (let i = 0; o < hiArray.length; i++){
+  ellipse(hiArray[i].x, hiArray[i].y,10, 10);}
+}
