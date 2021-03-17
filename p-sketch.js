276663a9f9696d334e4fb4983e31cbaa8af276da
hwajn <64378385+hwajn@users.noreply.github.com>
2021-03-17 00:19:35 -0400

    test

--- p-sketch.js
+++ p-sketch.js
@@ -1,38 +1,33 @@
-//https://editor.p5js.org/runemadsen/sketches/B1rJPV7n-
-
-
-let grotesk;
-let hiArray;
-letFontSize = 200;
-
-
+let font;
 function preload() {
-  grotesk = loadFont('grotesk.ttf');
-  grotesk = createFont("grotesk.ttf");
-  
+  font = loadFont('assets/inconsolata.ttf');
+}
+//https://p5js.org/reference/#/p5.Font/textToPoints
+let points;
+let bounds;
+function setup() {
+  createCanvas(100, 100);
+  stroke(0);
+  fill(255, 104, 204);
+
+  points = font.textToPoints('p5', 0, 0, 10, {
+    sampleFactor: 5,
+    simplifyThreshold: 0
+  });
+  bounds = font.textBounds(' p5 ', 0, 0, 10);
 }
 
-function setup() { 
-  createCanvas(1000, 1000);
-	noBackground();
-	fill(255,255,255);
-  stroke(0);
-	textSize(500);
-	textFont(grotesk);
-	textSize(fontSize);
-
-``
-  let hiArray = grotesk.textToPoints("L",width/2, height/2,fontSize);
-  print(hiArray);
-} 
-
-
-
-function draw(){
-  noBackground();
-	fill(255,255,255);
-  stroke(0);
-text("L",width/2, height/2);
-for (let i = 0; o < hiArray.length; i++){
-  ellipse(hiArray[i].x, hiArray[i].y,10, 10);}
+function draw() {
+  background(255);
+  beginShape();
+  translate(-bounds.x * width / bounds.w, -bounds.y * height / bounds.h);
+  for (let i = 0; i < points.length; i++) {
+    let p = points[i];
+    vertex(
+      p.x * width / bounds.w +
+        sin(20 * p.y / bounds.h + millis() / 1000) * width / 30,
+      p.y * height / bounds.h
+    );
+  }
+  endShape(CLOSE);
 }
